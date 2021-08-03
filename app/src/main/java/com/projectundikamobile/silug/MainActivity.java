c2ddcf23a350ab3d3d95cbae447e4f2f5b0779fd
fadillaoegi <fadilfdl2710@gmail.com>
2021-08-04 02:18:04 +0700

    break 10 menit

--- app/src/main/java/com/projectundikamobile/silug/MainActivity.java
+++ app/src/main/java/com/projectundikamobile/silug/MainActivity.java
@@ -2,7 +2,9 @@ package com.projectundikamobile.silug;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Handler Handler = new Handler();
+
+        Handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), login.class));
+                finish();
+            }
+        }, 4000L); // 4 detik
     }
 }
