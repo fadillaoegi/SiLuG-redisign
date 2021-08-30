fb756a8753b12c59098ab214880d7d3974876923
Fadil <fadilfdl2710@gmail.com>
2021-08-30 22:23:12 +0700

    push Sub menu

---
+++ app/src/main/java/com/projectundikamobile/silug/ipk.java
@@ -0,0 +1,14 @@
+package com.projectundikamobile.silug;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ipk extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ipk);
+    }
+}
